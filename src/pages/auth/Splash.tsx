d341296c387ebcef1794fcce020de7dde726a990
Mitchel Okoh <mitchelokoh@gmail.com>
2026-08-19 18:17:00 +0100

    fix: remove unused useEffect import in Splash.tsx

--- src/pages/auth/Splash.tsx
+++ src/pages/auth/Splash.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppLogo, PrimaryButton } from "../../components";
 import busesImg from "../../assets/buses.jpg";
